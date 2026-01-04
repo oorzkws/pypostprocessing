0623aba661f6ab8e77e7f6145dc2a3131875fb71
oorzkws <65210810+oorzkws@users.noreply.github.com>
2026-01-04 15:34:46 -0700

    Update datestamp.py

--- datestamp.py
+++ datestamp.py
@@ -1,11 +1,11 @@
 import json
 import os
 import re
-import subprocess
+from io import open
 from datetime import date
 
-INFO_PATH = f'info.json'
-CHANGELOG_PATH = f'changelog.txt'
+INFO_PATH = 'info.json'
+CHANGELOG_PATH = 'changelog.txt'
 
 def version_as_str(version: tuple[int]) -> str:
     str_tuple = map(str, version)
@@ -15,25 +15,30 @@ def str_as_version(version: str) -> tuple[int]:
     int_map = map(int, version.split('.'))
     return tuple(int_map)
 
-def get_changelog_version() -> tuple[int]:
-    version_regex = re.compile(r'Version: (?P<version>.*)$')
-    with open(CHANGELOG_PATH, 'r', encoding='utf-8') as changelog:
-        version_line = version_regex.match(changelog.read())
-        version_str = version_line.group('version')
-        return str_as_version(version_str)
+def read_file_content(file_path):
+    with open(file_path, 'r', encoding='utf-8', newline='') as read_file:
+        return read_file.read()
     
+def write_file_content(file_path, content):
+    with open(file_path, 'w', encoding='utf-8', newline='') as write_file:
+        return write_file.write(content)
+
+def replace_in_file(file_path, regexp, replacement):
+        content = read_file_content(file_path)
+        result = re.sub(regexp, replacement, content, count=1)
+        write_file_content(file_path, result)
+
+def get_changelog_version() -> tuple[int]:
+    content = read_file_content(CHANGELOG_PATH)
+    result = re.search(r'Version: (?P<version>.*)', content)
+    return str_as_version(result.group('version'))
+
 def set_changelog_version(new_version):
-    version_regex = re.compile(r'Version: (?P<version>.*)$')
-    with open(CHANGELOG_PATH, 'r+', encoding='utf-8', newline='') as changelog:
-        result = version_regex.sub(f'Version: {new_version}', changelog.read(), count=1)
-        changelog.write(result)
+    replace_in_file(CHANGELOG_PATH, r'Version: (?P<version>.*)', f'Version: {new_version}')
     
 def set_changelog_date():
-    date_regex = re.compile(r'Date: (?P<date>.*)$')
     cur_date = date.today().isoformat() # YYYY-MM-DD
-    with open(CHANGELOG_PATH, 'r+', encoding='utf-8', newline='') as changelog:
-        result = date_regex.sub(f'Date: {cur_date}', changelog.read(), count=1)
-        changelog.write(result)
+    replace_in_file(CHANGELOG_PATH, r'Date: (?P<date>.*)', f'Date: {cur_date}')
 
 def get_info_name() -> str:
     with open(INFO_PATH, 'r', encoding='utf-8', newline='') as info:
@@ -42,18 +47,17 @@ def get_info_name() -> str:
         return str_as_version(version_str) 
 
 def get_info_version() -> tuple[int]:
-    with open(INFO_PATH, 'r', encoding='utf-8', newline='') as info:
-        parsed_info = json.load(info)
-        version_str = parsed_info.get('version')
-        return str_as_version(version_str)
+    content = read_file_content(INFO_PATH)
+    info = json.loads(content)
+    version_str = info.get('version')
+    return str_as_version(version_str)
 
 def set_info_version(new_version: tuple[int]):
-      new_version_str = version_as_str(new_version)
-      with open(INFO_PATH, 'rw', encoding='utf-8', newline='') as info:
-        parsed_info = json.load(info)
-        old_version = parsed_info.get('version')
-        result = info.read().replace(old_version, new_version_str)
-        info.write(result)
+    new_version_str = version_as_str(new_version)
+    content = read_file_content(INFO_PATH)
+    info = json.loads(content)
+    old_version = info.get('version')
+    write_file_content(INFO_PATH, content.replace(old_version, new_version_str))
 
 def set_version_and_date():
     changelog_version = get_changelog_version()
